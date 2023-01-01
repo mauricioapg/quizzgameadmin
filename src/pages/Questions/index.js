c1841f28961f1d4858039402449d0f1643ebfcb7
Mauricio Aparecido Gabriel Dias <mauriciogabriel@MacBook-Pro-de-Mauricio.local>
2023-01-01 18:27:58 -0300

    implementando metodo GET para buscar perguntas

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,6 +1,6 @@
 import styles from './Questions.module.css'
-import { useState } from 'react'
-import questions from 'json/questions.json'
+import { useEffect, useState } from 'react'
+// import questions from 'json/questions.json'
 import QuestionCard from 'components/QuestionCard'
 import TextField from 'components/TextField'
 import DropDownList from 'components/DropDownList'
@@ -17,6 +17,23 @@ const Questions = () => {
     const [level, setLevel] = useState('')
     const [alternative, setAlternative] = useState('')
     const [alternatives, setAlternatives] = useState([])
+    const [questions, setQuestions] = useState([])
+
+    let headers = new Headers();
+    
+    headers.append('Authorization', 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXVyaWNpb2FwYXJlY2lkb2dhYnJpZWxAZ21haWwuY29tIiwiZXhwIjoxNjcyNjA5Mjg1fQ.fet2uVlkTDHTXccFRiL16NXqXadg62pi5KoHBydiBkRxkNbsqq9Q-SGZqF2b8P5-0ye2siI2f8_p1ucfY-DDiw');
+
+    useEffect(() => {
+        fetch('https://62edd141a785760e676f8b55.mockapi.io/api/v1/questions', {
+            method: 'GET',
+            headers: headers
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('DADOS AQUI: ' + data)
+                setQuestions(data)
+            })
+    }, [])
 
     return (
         <div>
